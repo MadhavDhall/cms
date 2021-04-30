edce412e2baf00afd44e7137ccbb3d1a99bcc493
Madhav <madhavdhall6@gmail.com>
2021-04-30 15:28:52 +0530

    better

--- app.js
+++ app.js
@@ -16,12 +16,12 @@ app.use(bodyParser.json())
 app.use("/cms-admin", require("./routes/admin"))
 app.use("/cms-admin/setup", require("./routes/setup/setup"))
 
-// if (process.env.NODE_ENV === "production") {
-app.use(express.static("./client/build"))
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
-// }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("./client/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 7000
 app.listen(port, () => {
